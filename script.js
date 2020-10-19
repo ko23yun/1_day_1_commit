74f59465cf934a2945b02154ac90baca0dbe5444
ko23yun <62507511+ko23yun@users.noreply.github.com>
2020-10-19 22:31:25 +0900

    201019_Commit_(Timer, Calculator)

--- script.js
+++ script.js
@@ -1,86 +1,57 @@
-const btn = document.querySelector(".btn");
-var x = document.getElementById("form");
-var form = document.getElementById("formValue");
+// 지금 숫자 입력하고 연산자 입력하면 숫자 따로 연산자 따로 분리 왼료시킴
+// 근데 연산자는 스트링에서 어떻게 연산자로 바꿀지는 잘 모르겠다. 오늘은 여기까지
 
-function startTimer(e) {
+const btn = document.querySelector(".btn");
+let x = document.querySelector(".form");
+const sum = document.querySelector(".sum");
+
+let LIST_NUM = [];
+let LIST_OPERATOR = [];
+
+function calNumber(e) {
   e.preventDefault();
-  var text = "";
+  let text = "";
   text = x.elements[0].value;
 
-  //   form.innerHTML = text;
-  var textTime = text;
+  let LIST_ITEM = text.split(" ");
 
-  writeTime(textTime);
-}
-
-function writeTime(val) {
-  var textTime = val;
-  //   console.log(textTime);
-
-  var time = textTime * 60;
-  var min = "";
-  var sec = "";
-
-  var x = setInterval(() => {
-    //   hour =
-    min = parseInt(time / 60);
-    sec = time % 60;
-
-    document.getElementById("demo").innerHTML = min + ":" + sec;
-    time--;
-
-    if (time < 0) {
-      clearInterval(x);
-
-      document.getElementById("demo").innerHTML = "시간초과";
+  for (let i = 0; i < LIST_ITEM.length; i++) {
+    if (i % 2 === 0) {
+      LIST_NUM.push(parseInt(LIST_ITEM[i], 10));
+      console.log(typeof parseInt(LIST_ITEM[i], 10));
+      console.log(LIST_NUM);
+    } else {
+      LIST_OPERATOR.push(LIST_ITEM[i]);
+      console.log(LIST_OPERATOR);
     }
-  }, 1000);
+  }
+
+  sum.innerHTML = LIST_ITEM;
 }
 
-// function function1()
+btn.addEventListener("click", calNumber);
+
 // {
-//   var variable1=12;
-//   function2(variable1);
+/* <form id="frm1">
+    First name: <input type="text" name="fname" value="Donald"><br>
+    Last name: <input type="text" name="lname" value="Duck"><br>
+    <input type="submit" value="submit">
+  </form>
+  <button onclick="myFunction()">Try</button>
+  <p id="demo"></p>
+</body>
+<script>
+  function myFunction(){
+    var x = document.getElementById("frm1");
+     //frm1에 해당하는 값들 배열형식으로 가져오기
+    var text="";
+    var i;
+    for(i=0;i<x.length;i++){
+      text+=x.elements[i].value + "<br>";
+       //x내의 value값을 뽑아내서 text에 저장
+    }
+    document.getElementById("demo").innerHTML = text;
+  }
+</script>
+  */
 // }
-
-// function function2(val)
-// {
-//   var variableOfFunction1 = val;
-
-btn.addEventListener("click", startTimer);
-
-// var time = text * 60;
-// var min = "";
-// var sec = "";
-
-// var x = setInterval(() => {
-//   min = parseInt(time / 60);
-//   sec = time % 60;
-
-//   document.getElementById("demo").innerHTML = min + "분" + sec + "초";
-//   time--;
-
-//   if (time < 0) {
-//     clearInterval(x);
-
-//     document.getElementById("demo").innerHTML = "시간초과";
-//   }
-// }, 1000);
-
-// var time = text;
-// var min = "";
-// var sec = "";
-
-// var x = setInterval(() => {
-//   min = parseInt(time / 60);
-//   sec = time % 60;
-
-//   document.getElementById("demo").innerHTML = min + "분" + sec + "초";
-//   time--;
-
-//   if (time < 0) {
-//     clearInterval(x);
-
-//     document.getElementById("demo").innerHTML = "시간초과";
-//   }
-// }, 1000);
